1714674b16bb37ee0b660db576002100b34206a3
Codemastr67 <lray.snow@gmail.com>
2026-01-08 20:36:23 -0500

    Update shrek.go

--- frames/shrek.go
+++ frames/shrek.go
@@ -1,1 +1,10 @@
+package frames
 
+// This is the value stored in the FrameMap
+var MyAnimation = DefaultFrameType(myAnimationFrames)
+
+var myAnimationFrames = []string{
+  `shrek1`,
+  `shrek2`,
+  `shrek3`,
+}
